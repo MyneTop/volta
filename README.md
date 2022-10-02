1be5b32708380a0ba7c31e2bb711f6375b998f1b
fourleaf <gundamwing.wei@gmail.com>
2022-10-03 00:37:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Weekly base volta update to provide the prebuilt binary for aarch-linux-gnu openssl-1.1
---
+++ runner/package.sh
@@ -0,0 +1,2 @@
+# package rust build result
+tar -czf volta-$EXT_RELEASE.tar.gz target/release/volta target/release/volta-migrate target/release/volta-shim
