ee2cda305a1156af217db67c42e1cf7bcac5d040
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 03:12:52 +0800

    more debug

--- runner/package.sh
+++ runner/package.sh
@@ -1,8 +1,11 @@
 # package rust build result
-ls -l /runner/_work/volta
-ls -l /runner/_work/volta/volta
-ls -l /runner/_work/volta/volta/volta
-echo $EXT_RELEASE
-# tar -czf volta-$EXT_RELEASE.tar.gz /runner/_work/volta/volta/volta \
-#  /runner/_work/volta/volta/volta-migrate \
-#  /runner/_work/volta/volta/volta-shim
+ls volta
+ls volta/target
+ls volta/target/release
+ls volta/target/release/volta
+echo "================================"
+echo ${steps.check-upstream.outputs.version}
+EXT_RELEASE=${steps.check-upstream.outputs.version}
+tar -czf volta-$EXT_RELEASE.tar.gz volta/target/release/volta/volta \
+ volta/target/release/volta/volta-migrate \
+ volta/target/release/volta/volta-shim
