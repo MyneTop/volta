8bb32e6e9fab6a84e182733b8ae25604aa609cbc
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 05:14:06 +0800

    rename release filename

--- runner/package.sh
+++ runner/package.sh
@@ -4,8 +4,8 @@ cp volta/target/aarch64-unknown-linux-gnu/release/volta pub
 cp volta/target/aarch64-unknown-linux-gnu/release/volta-migrate pub
 cp volta/target/aarch64-unknown-linux-gnu/release/volta-shim pub
 cd pub
-tar -czf volta-$VERSION.tar.gz *
-cp volta-$VERSION.tar.gz ../
+tar -czf volta-$VERSION-linux-aarch64-openssl-1.1.tar.gz *
+cp volta-$VERSION-linux-aarch64-openssl-1.1 ../
 # tar -czf volta-$VERSION.tar.gz volta/target/aarch64-unknown-linux-gnu/release/volta \
 #  volta/target/aarch64-unknown-linux-gnu/release/volta-migrate \
 #  volta/target/aarch64-unknown-linux-gnu/release/volta-shim
