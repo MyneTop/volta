de601dc217b9d6ab9b80899c6c67bc80f381ee4a
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 02:42:07 +0800

    try fix package

--- runner/package.sh
+++ runner/package.sh
@@ -1,2 +1,2 @@
 # package rust build result
-tar -czf volta-$EXT_RELEASE.tar.gz volta/target/release/volta volta/target/release/volta-migrate volta/target/release/volta-shim
+tar -czf volta-$EXT_RELEASE.tar.gz volta/volta volta/volta/crates/volta-migrate volta/volta/crates/volta-shim
