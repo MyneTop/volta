1c3959fb22b81e7a5b47d0c60934218a1b0b870c
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 05:11:29 +0800

    change tar path

--- runner/package.sh
+++ runner/package.sh
@@ -1,4 +1,11 @@
 # package rust build result
-tar -czf volta-$VERSION.tar.gz volta/target/aarch64-unknown-linux-gnu/release/volta \
- volta/target/aarch64-unknown-linux-gnu/release/volta-migrate \
- volta/target/aarch64-unknown-linux-gnu/release/volta-shim
+mkdir pub
+cp volta/target/aarch64-unknown-linux-gnu/release/volta pub
+cp volta/target/aarch64-unknown-linux-gnu/release/volta-migrate pub
+cp volta/target/aarch64-unknown-linux-gnu/release/volta-shim pub
+cd pub
+tar -czf volta-$VERSION.tar.gz *
+cp volta-$VERSION.tar.gz ../
+# tar -czf volta-$VERSION.tar.gz volta/target/aarch64-unknown-linux-gnu/release/volta \
+#  volta/target/aarch64-unknown-linux-gnu/release/volta-migrate \
+#  volta/target/aarch64-unknown-linux-gnu/release/volta-shim
