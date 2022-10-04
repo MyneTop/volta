4b1c7a31337b806a963e061436acafac8b95b9c9
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 03:35:06 +0800

    fix package

--- runner/package.sh
+++ runner/package.sh
@@ -1,12 +1,4 @@
 # package rust build result
-ls volta/target
-echo "================================"
-ls volta/target/release
-echo "================================"
-ls volta/target/release/build
-echo "================================"
-echo $VERSION
-EXT_RELEASE=${steps.check-upstream.outputs.version}
-tar -czf volta-$VERSION.tar.gz volta/target/release/build/volta \
- volta/target/release/build/volta-migrate \
- volta/target/release/build/volta-shim
+tar -czf volta-$VERSION.tar.gz volta/target/aarch64-unknown-linux-gnu/release/volta \
+ volta/target/aarch64-unknown-linux-gnu/release/volta-migrate \
+ volta/target/aarch64-unknown-linux-gnu/release/volta-shim
