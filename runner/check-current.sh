9c6d755cf7fc07634980ad07a15943aa2c045e1f
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 00:30:03 +0800

    fix vars

--- runner/check-current.sh
+++ runner/check-current.sh
@@ -1,3 +1,3 @@
 EXT_VERSION=$(curl -u ${{ secrets.CR_USER }}:${{ secrets.CR_PAT }} -sX GET https://api.github.com/repos/MyneTop/volta/releases/latest | jq -r '. | .tag_name' | sed 's|^v||')
 echo "upstream version: $EXT_VERSION"
-echo "::set-output name=version::$(EXT_VERSION)"
+echo "::set-output name=version::$EXT_VERSION"
--- runner/check-upstream.sh
+++ runner/check-upstream.sh
@@ -1,3 +1,3 @@
 EXT_RELEASE=$(curl -sX GET https://api.github.com/repos/volta-cli/volta/releases/latest | jq -r '. | .tag_name' | sed 's|^v||')
-echo "current version: $EXT_VERSION"
-echo "::set-output name=version::$(EXT_RELEASE)"
+echo "current version: $EXT_RELEASE"
+echo "::set-output name=version::$EXT_RELEASE"
