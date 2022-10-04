efda6a95a8a282dc8be3823d4b0cbab2be1e5ff2
fourleaf <gundamwing.wei@gmail.com>
2022-10-05 05:20:32 +0800

    fix package

--- runner/package.sh
+++ runner/package.sh
@@ -5,7 +5,7 @@ cp volta/target/aarch64-unknown-linux-gnu/release/volta-migrate pub
 cp volta/target/aarch64-unknown-linux-gnu/release/volta-shim pub
 cd pub
 tar -czf volta-$VERSION-linux-aarch64-openssl-1.1.tar.gz *
-cp volta-$VERSION-linux-aarch64-openssl-1.1 ../
+cp volta-$VERSION-linux-aarch64-openssl-1.1.tar.gz ../
 # tar -czf volta-$VERSION.tar.gz volta/target/aarch64-unknown-linux-gnu/release/volta \
 #  volta/target/aarch64-unknown-linux-gnu/release/volta-migrate \
 #  volta/target/aarch64-unknown-linux-gnu/release/volta-shim
